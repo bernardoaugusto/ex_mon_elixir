82176d036a97e87c0e2d661d2a5ffccefe8e72cd
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-21 08:35:41 -0300

    refactor: refactor Player struct

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,17 +1,4 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
   def hello do
     :world
   end
