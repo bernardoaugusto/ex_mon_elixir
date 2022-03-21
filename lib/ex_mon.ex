d89b7c776f2f179a96914332bc06665ac452681b
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-21 14:42:46 -0300

    feat: add game status

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,14 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Robotinik"
+
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  @spec start_game(any) :: {:error, any} | {:ok, pid}
+  def start_game(player) do
+    @computer_name |> create_player(:punch, :kick, :heal) |> Game.start(player)
   end
 end
