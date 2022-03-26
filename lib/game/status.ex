b5100b65069ec4fa55b3fa540443ffb7dc29aae7
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-25 22:32:15 -0300

    feat: print move message

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -7,7 +7,11 @@ defmodule ExMon.Game.Status do
     IO.puts("----------------------")
   end
 
-  def print_wrong_move_message(move) do
-    IO.puts("\n===== Invalid move: #{move} =====\n")
-  end
+  def print_wrong_move_message(move), do: IO.puts("\n===== Invalid move: #{move} =====\n")
+
+  def print_move_message(:computer, :attack, damage),
+    do: IO.puts("\n===== The Player attacked the computer dealing #{damage} damage =====\n")
+
+  def print_move_message(:player, :attack, damage),
+    do: IO.puts("\n===== The Computer attacked the player dealing #{damage} damage =====\n")
 end
