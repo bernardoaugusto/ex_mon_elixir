679808ad6510413c9c2aae96b87d4397eb1ef7d0
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-26 14:08:18 -0300

    test: add test Game.update/1

--- test/game_test.exs
+++ test/game_test.exs
@@ -37,4 +37,51 @@ defmodule ExMon.GameTest do
       assert expected_response == Game.info()
     end
   end
+
+  describe "update/1" do
+    test "returns the game state updated" do
+      player = Player.build("Bernardo", :punch, :kick, :heal)
+      computer = Player.build("Robotinick", :punch, :kick, :heal)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch},
+          name: "Robotinick"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch},
+          name: "Bernardo"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+
+      new_state = %{
+        computer: %ExMon.Player{
+          life: 85,
+          moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch},
+          name: "Robotinick"
+        },
+        player: %ExMon.Player{
+          life: 50,
+          moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch},
+          name: "Bernardo"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      Game.update(new_state)
+
+      expected_response = %{new_state | turn: :computer, status: :continue}
+
+      assert expected_response == Game.info()
+    end
+  end
 end
